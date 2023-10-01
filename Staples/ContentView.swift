d69a71f5ed870289e562a64e93ddd6d639d2e945
Gregory Kasbarian <greg@ghlk.net>
2023-10-01 06:34:50 -0700

    initial commit

--- Staples/ContentView.swift
+++ Staples/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+            Text("I want to make a grocery app that's helpful")
         }
         .padding()
     }
